33267c0d7c2eb28aba32e2a087a4df57461844c0
RamuPN <93154888+RamuPN@users.noreply.github.com>
2023-11-27 01:15:04 +0530

    Update streamlit_app.py
    Integrating Fruityvice API

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,10 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 fruits_to_show=my_fruit_list.loc[fruits_selected]
 #show table on page
 streamlit.dataframe(fruits_to_show)
+#display fruityviceapi_response
+
+streamlit.header("Fruityvice Fruit Advice!")
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
